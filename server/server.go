a2d7cf2241051af03a0e21efad17cccdb4e6ce9c
MrBStones <bjornmogelhoj@gmail.com>
2024-10-21 23:09:30 +0200

    need to have a different goroutine for broadcasting on the client

--- server/server.go
+++ server/server.go
@@ -1,10 +1,25 @@
 package main
 
 import (
+	pb "chittychat/stc"
 	"log"
+	"net"
+	"os"
+	"sync"
+
+	"google.golang.org/grpc"
 )
 
-func main() {
+type ChittyChatServer struct {
+	// type embedded to comply with Google lib
+	pb.UnimplementedChittyChatServer
+
+	lamporttime int
+	mu          sync.Mutex // protects chatMessages
+
+}
+
+func (s *ChittyChatServer) Chat(stream pb.ChittyChat_ChatServer) error {
 
 }
 
@@ -14,4 +29,26 @@ func broadcast() {
 
 func GetLamportTimestamp() int {
 	return 0
+}
+
+func main() {
+	var port string
+	if len(os.Args) > 1 {
+		port = os.Args[1]
+	} else {
+		port = ":3333"
+	}
+
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	chittyChatSyncServer := &ChittyChatServer{}
+	pb.RegisterChittyChatServer(s, chittyChatSyncServer)
+
+	log.Printf("servers listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
